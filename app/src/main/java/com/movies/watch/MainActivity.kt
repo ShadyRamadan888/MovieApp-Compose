5d7098b6282896bd810d2c89f773f77aae6644ee
Shady Ramadan <89228710+ShadyRamadan888@users.noreply.github.com>
2023-12-09 14:36:33 +0200

    fix: buildSrc required file

--- app/src/main/java/com/movies/watch/MainActivity.kt
+++ app/src/main/java/com/movies/watch/MainActivity.kt
@@ -3,7 +3,6 @@ package com.movies.watch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.movies.watch.ui.theme.WatchAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
