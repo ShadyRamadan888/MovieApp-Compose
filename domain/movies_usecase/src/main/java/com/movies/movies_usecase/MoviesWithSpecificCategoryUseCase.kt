2ba7c0acaabac2a5ddeec84092990f89d2c84185
Shady Ramadan <89228710+ShadyRamadan888@users.noreply.github.com>
2023-12-21 19:12:35 +0200

    refactor: Add internal modifier to make use case visible only within module

--- domain/movies_usecase/src/main/java/com/movies/movies_usecase/MoviesWithSpecificCategoryUseCase.kt
+++ domain/movies_usecase/src/main/java/com/movies/movies_usecase/MoviesWithSpecificCategoryUseCase.kt
@@ -4,7 +4,7 @@ import com.movies.model.movie.Movie
 import com.movies.repo.movies.MoviesRepo
 import kotlinx.coroutines.flow.Flow
 
-class MoviesWithSpecificCategoryUseCase(
+internal class MoviesWithSpecificCategoryUseCase(
     private val moviesRepo: MoviesRepo
 ) : IMoviesWithSpecificCategoryUseCase {
     override fun invoke(movieCategoryUrl: String): Flow<List<Movie>> =
