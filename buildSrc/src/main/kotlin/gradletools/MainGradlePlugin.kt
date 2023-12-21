4775a57926ee72300e07f000939889a655db07c5
Shady Ramadan <89228710+ShadyRamadan888@users.noreply.github.com>
2023-12-21 17:48:54 +0200

    chore: Add api_key and base_url to buildSrc gradle

--- buildSrc/src/main/kotlin/gradletools/MainGradlePlugin.kt
+++ buildSrc/src/main/kotlin/gradletools/MainGradlePlugin.kt
@@ -4,6 +4,7 @@ import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import java.util.Properties
 
 class MainGradlePlugin : Plugin<Project> {
 
@@ -24,6 +25,15 @@ class MainGradlePlugin : Plugin<Project> {
             compileOptions {
                 sourceCompatibility = JavaVersion.VERSION_17
                 targetCompatibility = JavaVersion.VERSION_17
+            }
+            defaultConfig {
+                val properties = Properties()
+                properties.load(project.rootProject.file("local.properties").inputStream())
+                buildConfigField("String", "API_KEY", "\"${properties.getProperty("API_KEY")}\"")
+                buildConfigField("String", "BASE_URL", "\"${properties.getProperty("BASE_URL")}\"")
+            }
+            buildFeatures {
+                buildConfig = true
             }
         }
     }
