8c9a3f0cca5da052d09320ba0aa9a16a94bdf2a9
Shady Ramadan <89228710+ShadyRamadan888@users.noreply.github.com>
2023-12-21 17:53:07 +0200

    chore: Add my modules paths to access easier

---
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -0,0 +1,9 @@
+object Modules {
+
+    const val app = ":app"
+
+    //data
+    const val dataModel = ":data:model"
+    const val dataService = ":data:service"
+    const val dataRepo = ":data:repo"
+}
