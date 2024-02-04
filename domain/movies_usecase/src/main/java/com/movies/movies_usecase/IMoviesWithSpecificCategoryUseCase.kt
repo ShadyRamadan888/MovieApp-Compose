6a60f9c5e78e0edad4372a27fb2d17f96491d011
Shady Ramadan <89228710+ShadyRamadan888@users.noreply.github.com>
2024-02-04 23:51:17 +0200

    refactor: project structure, remove unnecessary files

--- domain/movies_usecase/src/main/java/com/movies/movies_usecase/IMoviesWithSpecificCategoryUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.movies.movies_usecase
-
-import com.movies.model.movie.Movie
-import kotlinx.coroutines.flow.Flow
-
-interface IMoviesWithSpecificCategoryUseCase {
-    operator fun invoke(movieCategoryUrl: String): Flow<List<Movie>>
-}
