7315ed012edd4a9d85861d53f32a3a4430b89875
Shady Ramadan <shadymramadan5@gmail.com>
2024-08-15 16:25:24 +0300

    Fix unresolved files & set compose version

---
+++ domain/movies_usecase/build/generated/source/buildConfig/debug/com/movies/movies_usecase/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.movies.movies_usecase;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.movies.movies_usecase";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String API_KEY = "null";
+  // Field from default config.
+  public static final String BASE_URL = "null";
+}
